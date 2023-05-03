eae8c27a6133d16bc3aba1c314310f8e92a58e32
TrinidadBenegas <trinidadbenegas.estudio@gmail.com>
2023-05-03 16:57:30 -0300

    estructura inicial y dashboard

---
+++ argentour-app/argentour-app/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MiPerfilComponent } from './pages/mi-perfil/mi-perfil.component';
+import { BoletosComponent } from './pages/boletos/boletos.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { MetodosDePagoComponent } from './pages/metodos-de-pago/metodos-de-pago.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent, 
+children:[
+  
+   {path: 'perfil', component: MiPerfilComponent},
+   {path: 'boletos', component: BoletosComponent},
+   {path: 'favoritos', component: FavoritosComponent},
+   {path: 'metodos-de-pago', component: MetodosDePagoComponent}
+
+  ]},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
