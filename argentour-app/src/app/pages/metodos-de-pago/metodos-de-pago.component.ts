275ec507f5c2a331ec2cac7bf6e8b422354833fc
= <=>
2023-05-04 12:03:16 -0300

    incoporacion del index, nav, y footer

---
+++ argentour-app/src/app/pages/metodos-de-pago/metodos-de-pago.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metodos-de-pago',
+  templateUrl: './metodos-de-pago.component.html',
+  styleUrls: ['./metodos-de-pago.component.css']
+})
+export class MetodosDePagoComponent {
+
+}
